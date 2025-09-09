65c9d4557c2f0e871ef998bffa34f74dd9d0d3d8
actual-coding-god-josh <joshua.e.hoenig@gmail.com>
2025-09-09 22:51:52 +1000

    Update Database.py
    Before it was using a list of dictionaries, but what we want is a list of strings, where each string is a filename to a (e.g. character) folder

--- backend/database-structures/Database.py
+++ backend/database-structures/Database.py
@@ -3,7 +3,7 @@ from Event_Timeline_Graph import *
 class Database:
     def __init__(self):
         self.timeline: Timeline = Timeline()
-        self.characters: {str: [str]} = {}
-        self.places: {str: [str]} = {}
-        self.themes: {str: [str]} = {}
-        self.tags: [str] = []
+        self.characters: list[str] = []
+        self.places: list[str] = []
+        self.themes: list[str] = []
+        self.tags: list[str] = []
