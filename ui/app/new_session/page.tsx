e243160b4feb35c4ebc95d9771202d7c96e5e915
Jane L <l.jane1270@gmail.com>
2025-09-15 13:21:36 +1000

    jane: main merge

--- ui/app/new_session/page.tsx
+++ ui/app/new_session/page.tsx
@@ -1,9 +1,18 @@
 export default function newSession() {
   return (
-    <div className="flex items-center h-screen justify-center">
-      <div className="flex">
-        <button className="btn btn-outline">Upload Session</button>
-        <button className="btn btn-outline">Start Recording</button>
+    <div className="flex flex-col items-center">
+      <div>
+        <img
+          src="/svg/recording_page_dice.svg"
+          alt="D20"
+          className="w-1/2 h-auto" />
+      </div>
+
+      <div className="flex items-center h-screen justify-center">
+        <div className="flex">
+          <button className="btn btn-outline">Upload Session</button>
+          <button className="btn btn-outline">Start Recording</button>
+        </div>
       </div>
     </div>
   );
